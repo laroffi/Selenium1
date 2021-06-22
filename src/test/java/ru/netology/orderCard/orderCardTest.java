2ba021afe42157edd09cc9bfb60d469e72765455
Larisa Pylaeva <laralord@yandex.ru>
2021-06-22 13:44:13 +0300

    First commit

---
+++ src/test/java/ru/netology/orderCard/orderCardTest.java
@@ -0,0 +1,39 @@
+package ru.netology.orderCard;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class orderCardTest {
+    private WebDriver driver;
+    @BeforeAll
+    static void setUpAll() {
+        // убедитесь, что файл chromedriver.exe расположен именно в каталоге C:\tmp
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+    }
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestSomething() {
+        driver.get("http://localhost:9999");
+
+    }
+
+}
