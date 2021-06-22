def8eee1b7dda6c0ff07a5760b00664d2f00552f
Larisa Pylaeva <laralord@yandex.ru>
2021-06-22 19:12:12 +0300

    ?

--- src/test/java/ru/netology/orderCard/orderCardTest.java
+++ src/test/java/ru/netology/orderCard/orderCardTest.java
@@ -23,7 +23,6 @@ public class orderCardTest {
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
